79d6c2e840d265de65a83b0d0c9cb98ab25a7f18
danmasber <dmaserob02@gmail.com>
2018-05-09 21:54:17 +0200

    Add files via upload

---
+++ daniel_masero/session01/exercise-02/commands.bash
@@ -0,0 +1,28 @@
+#!/bin/bash 
+
+export MY_SANDBOX_IP=54.90.178.177
+export KUBECONFIG=~/.kube/sandbox.conf
+
+##Create namespace
+
+kubectl create ns exercise-02
+
+##Configure configmaps and secrets
+
+kubectl create -f cm.yaml                  
+kubectl create -f secrets.yaml
+
+
+##Configure the application
+
+kubectl create -f mariadb-deployment.yaml  
+kubectl create -f wordpress-deployment.yaml
+kubectl create -f wordpress-canary-deployment.yaml
+kubectl create -f mariadb-svc.yaml           
+kubectl create -f wordpress-svc.yaml
+kubectl create -f ingress.yaml             
+
+##Add to our /ect/hots an entry to make to the url
+
+echo "${MY_SANDBOX_IP}   wordpress-exercise-02.com" | sudo tee -a /etc/hosts
+
