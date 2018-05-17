df33745781468cefd00032001ee318ce8563c375
Pieter Temmerman <pieter@Pieters-MacBook-Air.local>
2018-05-17 02:42:45 +0200

    complete exercises

--- pieter_temmerman/session-02/exercise-03/commands.bash
+++ pieter_temmerman/session-02/exercise-03/commands.bash
@@ -8,7 +8,7 @@ kubectl delete namespace exercise-03
 
 # Create namespace
 sleep 15
-kubectl create namespace exercise-01
+kubectl create namespace exercise-03
 
 # Install LEMP chart
 helm install lemp
