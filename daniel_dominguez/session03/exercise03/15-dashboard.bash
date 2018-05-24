6298f31283c554e70a743c86f38d8d7530dbc8a3
Daniel Dominguez <ddoming@gmail.com>
2018-05-24 19:20:20 +0200

    Revert "exercise-03 step 1"
    This reverts commit e8e4ca8fc3242c169b6e0fe2ffc775cd5042374d.

--- daniel_dominguez/session03/exercise03/15-dashboard.bash
+++
@@ -1,4 +0,0 @@
-kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
-
-kubectl create clusterrolebinding --clusterrole=view --serviceaccount=kube-system:kubernetes-dashboard dashbordview
-
